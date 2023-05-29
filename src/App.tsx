9531dd13a89e2375019f31096fb6bf22e1ee185e
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-05-29 20:17:13 -0300

    Deleted copyright footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Recommendations from "./pages/Recommendations";
 import Timer from "./pages/Timer";
-import Copyright from "./components/Copyright";
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
           <Route path="/recommendations" element={<Recommendations />} />
           <Route path="/timer" element={<Timer />} />
         </Routes>
-        <Copyright />
       </Router>
     </>
   );
