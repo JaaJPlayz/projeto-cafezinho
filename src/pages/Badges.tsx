84b652555c5f9808e0eca8976345f2cf5e45beec
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-06-21 10:59:40 -0300

    Added badges

--- src/pages/Badges.tsx
+++ src/pages/Badges.tsx
@@ -4,7 +4,19 @@ const Badges = () => {
   return (
     <>
       <div className={styles.badges}>
-        <h1>Badges Page</h1>
+        <div className={styles.badgesContent}>
+          <div className={styles.badgeStand}>
+            <img src="/image/jason-icon.png" alt="jason-icon" width={"50px"} />
+          </div>
+
+          <div className={styles.badgeStand}>
+            <img src="/image/mario-icon.png" alt="jason-icon" width={"50px"} />
+          </div>
+
+          {Array.from({ length: 34 }).map((_, index) => (
+            <div key={index} className={styles.badgeStand}></div>
+          ))}
+        </div>
       </div>
     </>
   );
