c9f166a8648e5c180cc2099401950b9d90cb3d70
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-07-04 18:38:49 -0300

    Fixed infinite input in timer

--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -8,7 +8,7 @@ const Timer = () => {
 
   useEffect(() => {
     let totalSeconds = minutes * 60 + seconds;
-    let timer: any = null;
+    let timer;
 
     if (isTimerRunning && totalSeconds > 0) {
       timer = setInterval(() => {
@@ -25,51 +25,58 @@ const Timer = () => {
     };
   }, [minutes, seconds, isTimerRunning]);
 
+  const handleMinutesChange = (e) => {
+    const value = parseInt(e.target.value);
+    setMinutes(value > 59 ? 59 : value);
+  };
+
+  const handleSecondsChange = (e) => {
+    const value = parseInt(e.target.value);
+    setSeconds(value > 59 ? 59 : value);
+  };
+
   const handleClick = () => {
     setIsTimerRunning((prevIsTimerRunning) => !prevIsTimerRunning);
   };
 
   return (
-    <>
-      <div className={styles.timer}>
-        <div className={styles.timerContent}>
-          <div className={styles.minutes}>
-            <h1>{minutes.toString().padStart(2, "0")}</h1>
-            <p>Minutos</p>
-          </div>
-          <div className={styles.seconds}>
-            <h1>{seconds.toString().padStart(2, "0")}</h1>
-            <p>Segundos</p>
-          </div>
-          <div className={styles.inputsDiv}>
-            <input
-              type="number"
-              min="0"
-              max="59"
-              step="1"
-              placeholder="00"
-              id="minutes"
-              value={minutes}
-              onChange={(e) => setMinutes(parseInt(e.target.value))}
-            />
-            <input
-              type="number"
-              min="0"
-              max="59"
-              step="1"
-              placeholder="00"
-              id="seconds"
-              value={seconds}
-              onChange={(e) => setSeconds(parseInt(e.target.value))}
-            />
-            <button onClick={handleClick}>
-              {isTimerRunning ? "Stop" : "Start"}
-            </button>
-          </div>
+    <div className={styles.timer}>
+      <div className={styles.timerContent}>
+        <div className={styles.minutes}>
+          <h1>{minutes.toString().padStart(2, "0")}</h1>
+          <p>Minutos</p>
+        </div>
+        <div className={styles.seconds}>
+          <h1>{seconds.toString().padStart(2, "0")}</h1>
+          <p>Segundos</p>
+        </div>
+        <div className={styles.inputsDiv}>
+          <input
+            type="number"
+            min="0"
+            max="59"
+            step="1"
+            placeholder="00"
+            id="minutes"
+            value={minutes}
+            onChange={handleMinutesChange}
+          />
+          <input
+            type="number"
+            min="0"
+            max="59"
+            step="1"
+            placeholder="00"
+            id="seconds"
+            value={seconds}
+            onChange={handleSecondsChange}
+          />
+          <button onClick={handleClick}>{isTimerRunning ? "Stop" : "Start"}</button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
 export default Timer;
+
